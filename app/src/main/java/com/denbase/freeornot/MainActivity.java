67f6be38dd64fc1cf027df312d35abbe16ad42cf
Emre <emre10sahin10@gmail.com>
2020-12-24 22:07:44 +0300

    Yasak mi Serbest mi kontrolu

--- app/src/main/java/com/denbase/freeornot/MainActivity.java
+++ app/src/main/java/com/denbase/freeornot/MainActivity.java
@@ -89,7 +89,6 @@ public class MainActivity extends AppCompatActivity {
                             imgSonuc.setImageResource(R.drawable.ic_yasak);
                         }
                     }
-
                 }
             }
         });
